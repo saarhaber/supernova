0098d07eb69c0c0fa3a6fa917a21369a9f986d4e
saarhaber <saarhaber@gmail.com>
2020-09-19 15:36:34 -0400

    All

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import React, { useState} from 'react';
 import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  const [outputText, setOutputText] = useState('Open up App.js to start working on your app!', )
+  const [outputText, setOutputText] = useState('Open up App.js to start working on your app !', )
   return (
     <View style={styles.container}>
       <Text>{outputText}</Text>
