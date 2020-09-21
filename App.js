54fd880dd5615096b712020d5bc9c8e022d75ac4
Saar Haber <37675905+saarhaber@users.noreply.github.com>
2020-09-20 20:50:04 -0400

    starting with navigation

--- App.js
+++ App.js
@@ -1,21 +1,39 @@
-import React from 'react';
+import * as React from 'react';
 import { Button, StyleSheet, Text, View, TextInput } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-export default function App() {
+function HomeScreen() {
   return (
-    <View style={{padding: 50}}>
-      <View>
-        <TextInput placeholder="NYT Best Sellers" style={{padding: 10, fontSize: 30, textAlign: "center"}}/>
-      </View>
-      <View >
-        <Button title="History" />
-        <Button title="Scan" />
-        <Button title="Favorite" />
-      </View>
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
- 
-});
+const Stack = createStackNavigator();
+
+ function App() {
+  return (
+    <NavigationContainer>
+    <Stack.Navigator>
+      <Stack.Screen name="Home" component={HomeScreen} />
+    </Stack.Navigator>
+    </NavigationContainer>
+    // <View style={{padding: 50}}>
+    //   <View>
+    //     <TextInput placeholder="NYT Best Sellers" style={{padding: 10, fontSize: 30, textAlign: "center"}}/>
+    //   </View>
+    //   <View >
+    //     <Button title="History" />
+    //     <Button title="Scan" />
+    //     <Button title="Favorite" />
+    //   </View>
+    // </View>
+  );
+}
+
+// const styles = StyleSheet.create({
+// });
+
+export default App;
