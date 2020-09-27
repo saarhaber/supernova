be70e6a98f93597cc85f2fd7bceb8adad12eaec6
Saar Haber <37675905+saarhaber@users.noreply.github.com>
2020-09-26 20:22:33 -0400

    stuck at response

--- screens/Search.js
+++ screens/Search.js
@@ -1,5 +1,6 @@
 import * as React from 'react';
-import { Button, View, Text } from 'react-native';
+import { Button, View, Text, ScrollView, Dimensions} from 'react-native';
+import { TextInput } from 'react-native-gesture-handler';
 
 /*
 key: l2sRuWFpQfI2HFPSm96ZmQ
@@ -7,16 +8,49 @@ secret: 65YExjsKFQJaDmsuFmT7aRcVbpvTgMcIABO49lxM3NE
 */
 
 class Search extends React.Component {
-    render()
-    {
+    constructor(props) {
+        super(props)
+        this.state = {
+            key:"l2sRuWFpQfI2HFPSm96ZmQ",
+            inputIsbn: '',
+            reviews: null
+        }
+    }
+    
+    handleSubmit() {
+        fetch(`https://www.goodreads.com/book/isbn/${this.inputIsbn}.json?key=${this.key}`, {
+            method: 'GET'
+        })
+          .then ((response) => response.text())
+          .then ((json) => {
+              console.log(json)
+            //   this.setState({reviews: json.book.reviews_widget});
+          })
+            .catch((err) => {
+                console.log(err);
+            });
+    }
+
+
+    render() {
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
         <Text>Search Page</Text>
+        <TextInput placeholder = {"Enter ISBN number"}
+        onChangeText={(text) => this.setState({inputIsbn: text})}></TextInput>
+        <Button
+          title="Submit"
+          onPress={() =>  this.handleSubmit()}
+        />
         <Button
           title="Home"
           onPress={() => this.props.navigation.navigate('Home')}
         />
+        <View>
+            <Text>{this.state.reviews}</Text>
+        </View>
       </View>
+       
     );
   }
 }
