831439872a7e8147d561c8d95690617db9a7b031
soopz <pixelman65@gmail.com>
2020-10-17 19:12:43 -0400

    Scan navigates back to search

--- screens/Scan.js
+++ screens/Scan.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { Text, View, StyleSheet, Button, Image } from 'react-native';
 import { BarCodeScanner } from 'expo-barcode-scanner';
 
-export default function App() {
+export default function App({navigation}) {
   const [hasPermission, setHasPermission] = useState(null);
   const [scanned, setScanned] = useState(false);
 
@@ -17,6 +17,10 @@ export default function App() {
     setScanned(true);
     alert(`Bar code with type ${type} and data ${data} has been scanned!`);
   };
+  function AfterScanned(){
+    setScanned(false);
+    navigation.navigate('Search');
+  }
 
   if (hasPermission === null) {
     return <Text>Requesting for camera permission</Text>;
@@ -39,7 +43,7 @@ export default function App() {
     <Image style = {{width:500 ,height:500 }} source = {{uri:'https://i.stack.imgur.com/VVqSa.png'}}/>
     </View>
     <View style = {{flex:1,flexDirection:'column',justifyContent:'space-evenly'}}>
-    {scanned && <Button title={'Tap to Scan Again'} onPress={() => setScanned(false)} />}
+    {scanned && <Button title={'Tap to Go Back'} onPress={() => AfterScanned()} />}
     </View>
     </View>
   );
