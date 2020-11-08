ce4aa6cbc68670dff9b1ed681ba6d3d4fe85c9bb
mestrella740 <65213532+mestrella740@users.noreply.github.com>
2020-11-08 15:53:39 -0500

    Update Bookevents.js

--- screens/Bookevents.js
+++ screens/Bookevents.js
@@ -1,18 +1,90 @@
 import * as React from 'react';
-import { Button, View, Text } from 'react-native';
+import { Image, StyleSheet, Text, TouchableOpacity, View, Button, ScrollView } from 'react-native';
 
-class Bookevents extends React.Component {
-    render()
-    {
+export default class Bookevents extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      eventsList: [],
+    };
+  }
+
+  componentDidMount() {
+    this.fetchData();
+  }
+  fetchData = async () => {
+    const api_url =
+      'https://booksigningevent.com/wp-json/tribe/events/v1/events';
+    const response = await fetch(api_url);
+    const data = await response.json();
+    this.setState({ eventsList: data.events });
+  };
+
+  render() {
     return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Book Signing Events Page</Text>
-        <Button
-          title="Home"
-          onPress={() => this.props.navigation.navigate('Home')}
-        />
+      <View>
+
+        <View>
+          <Text style={styles.pageHeader}>Upcoming Events!</Text>
+        </View>
+
+        <View>
+          <FlatList
+            data={this.state.eventsList}
+            keyExtractor={(item) => item.id}
+            renderItem={({ item }) => (
+              <View>
+                <Text style={styles.eventHeader}>
+                  <Image style={styles.pics} source={{ uri: item.image.url }} />
+                  <Text style={styles.boldText}>{item.title})</Text>
+                  {'\n'}
+                  <Text style={styles.boldText}>Date: </Text>{item.start_date}
+                  {'\n'}
+                  <Text style={styles.boldText}>Location: </Text>{item.venue.venue}
+                </Text>
+                <Text style={styles.eventContent}>{item.description}</Text>
+              </View>
+            )}
+          />
+        </View>        
+        
       </View>
     );
   }
 }
-export default Bookevents;
+
+const styles = StyleSheet.create({
+  pageHeader: {
+    color: 'white',
+    backgroundColor: 'black',
+    textAlign: 'center',
+    padding: 20,
+    fontSize: 20,
+    borderWidth: 1,
+    marginBottom: 10,
+  },
+  eventHeader: {
+    backgroundColor: 'skyblue',
+    padding: 20,
+    borderColor: 'black',
+    borderWidth: 1,
+  },
+  eventContent: {
+    backgroundColor: 'lightgrey',
+    padding: 10,
+    alignItems: 'right',
+    justifyContent: 'right',
+    marginBottom: 10,
+  },
+  pics: {
+    width: 75,
+    height: 75,
+    marginLeft: 5,
+    float: 'right',
+  },
+  boldText: {
+    fontWeight: 'bold',
+  },
+});
+
+
