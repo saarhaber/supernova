43059b853a291e83349a50b97a173f361f91d5e3
mestrella740 <65213532+mestrella740@users.noreply.github.com>
2020-11-11 05:26:32 -0500

    Update Bookevents.js
    Fixed text display bug
    Cleaned things up
    Should be fully functional now

--- screens/Bookevents.js
+++ screens/Bookevents.js
@@ -34,32 +34,25 @@ export default class Bookevents extends React.Component {
             keyExtractor={(item) => item.id}
             renderItem={({ item }) => (
               <View>
-                <Text style={styles.eventHeader}>
-                  <View style={{ flexDirection: 'row'}}>
-                  <Image
-                      style={styles.pics}
-                      source={{ uri: item.image.url }}
-                    />
-                    <View style={{ flex: 10, flexDirection: 'column', paddingLeft: 10 }}>
-                      <Text style={styles.boldText}>
-                        {item.title
-                          .replace(/&#(\d{4};)/gi, '')
-                          .replace(/&#(\d{3};)/gi, '')}
-                      </Text>
+                <View style={styles.eventHeader}>
 
-                      <Text style={styles.boldText}>Date: </Text>
-                      <Text>{item.start_date}</Text>
-                      <Text style={styles.boldText}>Location: </Text>
-                      <Text>
-                        {item.venue.venue
-                          .replace(/&#(\d{4};)/gi, '')
-                          .replace(/&#(\d{3};)/gi, '')}
-                      </Text>
-                    </View>
-
-                    
-                  </View>
-                </Text>
+                  <Text style={styles.eventHeaderText}>
+                    <Text style={styles.boldText}>Event:</Text>{' '}
+                      {item.title
+                        .replace(/&#(\d{4};)/gi, '')
+                        .replace(/&#(\d{3};)/gi, '')}{'\n'}
+                    <Text style={styles.boldText}>Date:</Text>{' '}
+                    {item.start_date_details.month}/
+                    {item.start_date_details.day}/{item.start_date_details.year}{'\n'}
+                    <Text style={styles.boldText}>Location: </Text>
+                    {item.venue.venue
+                      .replace(/&#(\d{4};)/gi, '')
+                      .replace(/&#(\d{3};)/gi, '')}
+                  </Text>
+                  
+                  <Image style={styles.pics} source={{ uri: item.image.url }} />
+                  
+                </View>
 
                 <Text style={styles.eventContent}>
                   {item.description
@@ -85,25 +78,31 @@ const styles = StyleSheet.create({
     fontSize: 20,
     borderWidth: 1,
     marginBottom: 10,
+    fontWeight: 'bold',
   },
   eventHeader: {
     backgroundColor: 'skyblue',
     padding: 20,
     borderColor: 'black',
     borderWidth: 1,
+    flexDirection: 'row',
+//  justifyContent: 'space-between',
+  },
+  eventHeaderText: {
+    flex: 3,
+//  flexDirection: 'row',
+  },
+  pics: {
+    width: 100,
+    height: 100,
+//  flex: 1,
   },
   eventContent: {
     backgroundColor: 'lightgrey',
     padding: 10,
     marginBottom: 10,
   },
-  pics: {
-    width: 100,
-    height: 100,
-    flex: 6,
-  },
   boldText: {
     fontWeight: 'bold',
-    flex: 1,
   }
 });
